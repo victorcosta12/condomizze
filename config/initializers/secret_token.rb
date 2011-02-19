4582aa36e604ec443edbc762e783e89c5330cf23
michel_do3 <michel_do3@hotmail.com>
2011-02-18 23:44:46 -0200

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Condomizze::Application.config.secret_token = '39ffc5b4844a42a0d5eaf99e8c7233c2db5cacd16d233c23f819daace11071ec7dae83aed463862329f6b0b28e8d5144257dca176b87be750af38b707cd62c7c'
